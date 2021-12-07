e600046136744952d04c40946cc127692128c647
Andjela Kostic <andjelakostic110@gmail.com>
2021-12-07 15:52:35 +0100

    Kreirana index.php strana

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, intitial-scale=1.0">
+    <title>Putovanja</title>
+    <link rel="stylesheet" type="text/css" href="css/pocetna.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" />
+</head>
+
+<body>
+
+    <?php
+    include 'zaglavlja/header.php';
+    ?>
+
+    <div id="pocetna" class="row strana" style="background-color: #e6c4bc;">
+        <div class="col-md-6">
+            <img src="slike/naslovna.png" class="img-fluid" alt="">
+        </div>
+        <div class="col-md-6">
+            <div class="row">
+                <img src="slike/slovanaslovna.png" class="img-fluid" alt="">
+            </div>
+            <div id="tekstDobrodosli" class="row">
+                <p>Dobrodošli! Pri kreiranju sajta želja mi je bila da vam otkrijem neke manje ili potpuno neistražene delove gradova, da kroz naša putovanja posećujemo mnoga mesta gde ide lokalno stanovništvo, da doživite gradove na pravi način. Da uđete u njihovu dušu. Tokom cele godine tu su putovanja u najveće evropske metropole kao što su: Rim, Firenca, Budimpešta, Prag, Barselona, Istanbul. Biće šetnji, uživanja u lokalnoj kuhinji, upoznavanja sa kulturom i običajima, šopinga- za svakoga ponešto. Cilj mi je da putovanja budu pristupačna za svakoga, da svako uživa u lepotama i raznolikosti ovog sveta. Tu sam da vaše želje i potrebe pretočim u realnost. Dozvolite sebi da prošetate neturističkim mestima. Bićete oduševljeni! -Anđela</p>
+            </div>
+        </div>
+    </div>
+
+
+    <?php
+    include 'zaglavlja/footer.php';
+    ?>
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"></script>
+</body>
+
+</html>
