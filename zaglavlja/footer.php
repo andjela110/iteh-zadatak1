b0c5a3a53ea60001801d8f3540838a8f82678d9d
Andjela Kostic <andjelakostic110@gmail.com>
2021-12-07 15:48:23 +0100

    Kreirani header i footer

---
+++ zaglavlja/footer.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <footer class=" text-dark pt-5 pb-4" style="background-color: #bdbdbd;">
+
+        <div class="container text-center text-md-left">
+
+            <div class="row text-center text-md-left">
+
+                <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                    <h5 class="text-uppercase mb-4 font-weight-bold text-secondary">O nama</h5>
+                    <hr class="mb-4">
+                    <p>Ćao, ja sam Anđela. Ovaj program nastao je iz želje da podelim sa vama otkrivene čarobne kutke u Evropi. Cilj mi je da putovanja budu pristupačna za svakoga, da svako uživa u lepotama i raznolikosti ovog sveta. </p>
+
+                </div>
+
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                    <h5 class="text-uppercase mb-4 font-weight-bold text-secondary">Države</h5>
+                    <hr class="mb-4">
+                    <p>
+                        <a href="#" class="text-dark" style="text-decoration: none;"> Srbija</a>
+                    </p>
+                    <p>
+                        <a href="#" class="text-dark" style="text-decoration: none;"> Španija</a>
+                    </p>
+                    <p>
+                        <a href="#" class="text-dark" style="text-decoration: none;"> Turska</a>
+                    </p>
+                    <p>
+                        <a href="#" class="text-dark" style="text-decoration: none;"> Italija</a>
+                    </p>
+
+                </div>
+
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                    <h5 class="text-uppercase mb-4 font-weight-bold text-secondary">Gradovi</h5>
+                    <hr class="mb-4">
+                    <p>
+                        <a href="#" class="text-dark" style="text-decoration: none;"> Vranje, Ćuprija, Niš..</a>
+                    </p>
+                    <p>
+                        <a href="#" class="text-dark" style="text-decoration: none;"> Barselona, Madrid, Malaga..</a>
+                    </p>
+                    <p>
+                        <a href="#" class="text-dark" style="text-decoration: none;"> Istanbul, Čanakale, Izmir..</a>
+                    </p>
+                    <p>
+                        <a href="#" class="text-dark" style="text-decoration: none;"> Firenca, Venecija, Rim..</a>
+                    </p>
+                </div>
+
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                    <h5 class="text-uppercase mb-4 font-weight-bold text-secondary"> Kontakt</h5>
+                    <hr class="mb-4">
+                    <p>
+                        <i class="fas fa-home mr-3"></i> Beograd, 11010, Srbija
+                    </p>
+                    <p>
+                        <i class="fas fa-envelope mr-3"></i> andjelakostic110@gmail.com
+                    </p>
+                    <p>
+                        <i class="fas fa-phone mr-3"></i> +381 69101010
+                    </p>
+                    <p>
+                        <i class="fas fa-print	 mr-3"></i> +381 69101010
+                    </p>
+                </div>
+
+            </div>
+
+            <hr class="mb-4">
+            <div class=" row d-flex justify-content-center">
+                <div>
+                    <p> Copyright ©2021 Sva prava rezervisana:
+                        <a href="#" style="text-decoration: none;">
+                            <strong class="text-secondary">Anđela Kostić</strong>
+                        </a>
+                    </p>
+                </div>
+            </div>
+            <div class="row d-flex justify-content-center">
+                <div class="text-center ">
+
+                    <ul class="list-unstyled list-inline">
+                        <li class="list-inline-item">
+                            <a href="#" class="text-dark"><i class="fab fa-facebook"></i></a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#" class="text-dark"><i class="fab fa-twitter"></i></a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#" class=" text-dark"><i class="fab fa-google-plus"></i></a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#" class=" text-dark"><i class="fab fa-linkedin-in"></i></a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#" class=" text-dark"><i class="fab fa-youtube"></i></a>
+                        </li>
+                    </ul>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        </div>
+</body>
+
+</html>
---
+++ zaglavlja/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <div class="collapse" id="navbarToggleExternalContent">
+        <div class="p-4" style="background-color: #bdbdbd;">
+            <a style="text-decoration:none" href="index.php">
+                <h5 class="text-white h4">Početna</h5>
+            </a>
+            <span class="text-muted">Dobrodošli, uživajte</span>
+            <a style="text-decoration:none" href="login.php">
+                <h5 class="text-white h4">Logovanje</h5>
+            </a>
+            <span class="text-muted">Ulogujte se i pridružite nam se u čarobnim putovanjima</span>
+            <a style="text-decoration:none" href="glavna.php">
+                <h5 class="text-white h4">Prijava za putovanje</h5>
+            </a>
+            <span class="text-muted">Prijavite se, nove avanture čekaju na vas</span>
+
+        </div>
+    </div>
+    <nav class="navbar navbar-light" style="background-color: #bdbdbd;">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+        </div>
+    </nav>
+</body>
+
+</html>
